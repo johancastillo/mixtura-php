875b0e68ded7d6ea1d869e6a5e2c6191aaf4e7dc
Johan Castillo <jcjohan2707@gmail.com>
2020-12-25 21:13:36 -0400

    Navegation Component Init

---
+++ components/Navegation.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="menu_bar">
+        <a href="#" class="bt-menu">
+            <img src="./assets/img/logo.svg" width="50" alt="">
+        </a>
+
+        <span class="icon-menu menu"></span>
+    </div>
+
+    <nav>
+        <ul>
+            <li><a href="#"><span class="icon-home"></span>Inicio</a></li>
+            <li><a href="#"><span class="icon-suitcase"></span>Menú</a></li>
+            <li><a href="#"><span class="icon-rocket"></span>Enlace</a></li>
+            <li><a href="#"><span class="icon-earth"></span>Servicios</a></li>
+            <li><a href="#"><span class="icon-mail"></span>Contacto</a></li>
+        </ul>
+    </nav>
+</header>
---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es-ve">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Mixtura</title>
+
+    <!-- CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    
+    <link rel="stylesheet" href="./assets/css/main.css">
+    <link rel="stylesheet" href="./assets/icomoon/style.css">
+</head>
+<body>
+    <!-- Navegation Start -->
+    <?php include "./components/Navegation.php" ?>
+    <!--  Navegation End -->
+
+
+    
+    <script src="./assets/js/jquery-latest.js"></script>
+    <script src="./assets/js/navegation.js"></script>
+</body>
+</html>
